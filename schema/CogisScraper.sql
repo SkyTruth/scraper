5f67aa1ff932a9903612ca1266aa0f6007a6e992
Craig Winters <craigwin.ni@gmail.com>
2013-01-22 15:54:08 -0600

    Add field ft_id for syncing with Fusion Table. Change field 'id' to 'st_id' to avoid confusion.

--- schema/CogisScraper.sql
+++ schema/CogisScraper.sql
@@ -17,13 +17,13 @@
 
 --
 -- Table structure for table `cogisinspection`
--
+--
 
 DROP TABLE IF EXISTS `cogisinspection`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cogisinspection` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `st_id` int(11) NOT NULL AUTO_INCREMENT,
   `doc_num` varchar(15) NOT NULL,
   `county_code` varchar(10) DEFAULT NULL,
   `county_name` varchar(30) DEFAULT NULL,
@@ -40,13 +40,13 @@ CREATE TABLE `cogisinspection` (
   `site_lat` varchar(20) DEFAULT NULL,
   `site_lng` varchar(20) DEFAULT NULL,
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  `ft_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`st_id`),
   KEY `doc_num_index` (`doc_num`),
   KEY `lat` (`site_lat`),
   KEY `lng` (`site_lng`)
-) ENGINE=InnoDB AUTO_INCREMENT=2557 DEFAULT CHARSET=utf8 COMMENT='COGIS well inspection records';
+) ENGINE=InnoDB AUTO_INCREMENT=2789 DEFAULT CHARSET=utf8 COMMENT='COGIS well inspection records';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 
 --
 -- Table structure for table `cogisspill`
@@ -56,7 +56,7 @@ DROP TABLE IF EXISTS `cogisspill`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cogisspill` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `st_id` int(11) NOT NULL AUTO_INCREMENT,
   `doc_num` varchar(15) NOT NULL,
   `county_code` varchar(10) DEFAULT NULL,
   `county_name` varchar(30) DEFAULT NULL,
@@ -72,21 +72,12 @@ CREATE TABLE `cogisspill` (
   `spill_lat` varchar(20) DEFAULT NULL,
   `spill_lng` varchar(20) DEFAULT NULL,
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idcogisspill_UNIQUE` (`id`),
+  `ft_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`st_id`),
+  UNIQUE KEY `idcogisspill_UNIQUE` (`st_id`),
   KEY `doc_num_index` (`doc_num`),
   KEY `lat` (`spill_lat`),
   KEY `lng` (`spill_lng`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=283 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-01-11  8:11:56
