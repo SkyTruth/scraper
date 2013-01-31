8d6fbb7a04761de080e6f372cbfc89a96ca38322
Craig Winters <craigwin.ni@gmail.com>
2013-01-31 15:37:47 -0600

    Tables BotJob and BotJobParams

---
+++ schema/JobBot_schema.sql
@@ -0,0 +1,49 @@
+-- MySQL dump 10.13  Distrib 5.5.28, for Win32 (x86)
+--
+-- Host: localhost    Database: nrc_data3
+-- ------------------------------------------------------
+-- Server version	5.5.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `botjob`
+--
+
+DROP TABLE IF EXISTS `botjob`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `botjob` (
+  `job_id` int(11) NOT NULL,
+  `job_name` varchar(60) NOT NULL,
+  `job_bot` varchar(45) NOT NULL,
+  PRIMARY KEY (`job_id`),
+  UNIQUE KEY `job_name_UNIQUE` (`job_name`),
+  UNIQUE KEY `job_id_UNIQUE` (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `botjobparams`
+--
+
+DROP TABLE IF EXISTS `botjobparams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `botjobparams` (
+  `job_id` int(11) NOT NULL,
+  `key` varchar(45) NOT NULL,
+  `value` varchar(1024) NOT NULL DEFAULT '',
+  PRIMARY KEY (`job_id`,`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
