abc70e78b7a7f818075903d3e108ebbb4dc3e55e
Kevin Wurster <kevin@skytruth.org>
2014-08-11 16:16:52 -0400

    Establish DB connection, open workbook, and define field map

--- bin/nrcSpreadsheetScraper.py
+++ bin/nrcSpreadsheetScraper.py
@@ -37,17 +37,22 @@ Scraper for the "temporary" NRC incident spreadsheet
 """
 
 
+from __future__ import division
 from __future__ import print_function
 
+import json
 from os.path import *
+import psycopg2
 import sys
+import xlrd
 
 
 #/* ======================================================================= */#
 #/*     Document level information
 #/* ======================================================================= */#
 
-__all__ = ['__version__', '__release__', '__author__', '__source__', '__package__', '__license__']
+__all__ = ['__version__', '__release__', '__author__', '__source__', '__package__', '__license__',
+           'print_usage', 'print_license', 'print_help', 'print_help_info', 'print_version', 'main']
 
 
 #/* ======================================================================= */#
@@ -197,6 +202,36 @@ def print_version():
 
 
 #/* ======================================================================= */#
+#/*     Define dms2dd() function
+#/* ======================================================================= */#
+
+def dms2dd(degrees, minutes, seconds, quadrant):
+
+    """
+    Convert degrees, minutes, seconds, quadrant to decimal degrees
+
+    :param degrees: coordinate degrees
+    :type degrees: int
+    :param minutes: coordinate minutes
+    :type minutes: int
+    :param seconds: coordinate seconds
+    :type seconds: int
+    :param quadrant: coordinate quadrant (N, E, S, W)
+    :type quadrant: str
+
+    :return: decimal degrees
+    :rtype: float
+    """
+
+    output = degrees + (minutes / 60) + (seconds / 3600)
+
+    if quadrant.lower() in ('s', 'w'):
+        output *= -1
+
+    return output
+
+
+#/* ======================================================================= */#
 #/*     Define main() function
 #/* ======================================================================= */#
 
@@ -212,19 +247,208 @@ def main(args):
     :rtype: int
     """
 
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
     #/*     Defaults
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
 
+    # NRC file
     download_url = 'http://cgmix.uscg.mil/NRC/FOIAFiles/Current.xlsx'
+    file_to_process = basename(download_url)
 
-    #/* ======================================================================= */#
+    # DB connection parameters
+    db_host = 'ewn3'
+    db_name = 'skytruth'
+    db_user = 'scraper'
+    db_pass = ''
+
+    #/* ----------------------------------------------------------------------- */#
     #/*     Containers
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
 
-    #/* ======================================================================= */#
+    db_connection_string = None
+
+    # Field map describing which field on which sheet maps to which field in which table
+    field_map_workbook_to_db = [
+
+        # -={ Table: NrcScrapedReport }=- #
+        {
+            'sheet': 'CALLS',
+            'field': 'SEQNOS',
+            'db_field': 'reportnum',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'CALLS',
+            'field': 'DATE_TIME_RECEIVED',
+            'db_field': 'received_datetime',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'CALLS',
+            'field': 'CALLTYPE',
+            'db_field': 'calltype',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'CALLS',
+            'field': 'RESPONSIBLE_COMPANY',
+            'db_field': 'suspected_responsible_company',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'DESCRIPTION_OF_INCIDENT',
+            'db_field': 'description',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'INCIDENT_DATE_TIME',
+            'db_field': 'incident_datetime',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'TYPE_OF_INCIDENT',
+            'db_field': 'incidenttype',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'INCIDENT_CAUSE',
+            'db_field': 'cause',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'LOCATION_ADDRESS',
+            'db_field': 'location',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'LOCATION_STATE',
+            'db_field': 'state',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'LOCATION_NEAREST_CITY',
+            'db_field': 'nearestcity',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'LOCATION_COUNTY',
+            'db_field': 'county',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+        {
+            'sheet': 'INCIDENT_DETAILS',
+            'field': 'MEDIUM_DESC',
+            'db_field': 'medium_affected',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedReport'
+        },
+
+        # -={ Table: NrcParsedReport }=- #
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'SEQNOS',
+            'db_field': 'reportnum',
+            'db_schema': 'public',
+            'db_table': 'NrcParsedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'LOCATION_ZIP',
+            'db_field': 'zip',
+            'db_schema': 'public',
+            'db_table': 'NrcParsedReport'
+        },
+        {
+            'sheet': 'INCIDENT_COMMONS',
+            'field': 'INCIDENT_COUNTY',
+            'db_field': 'county',
+            'db_schema': 'public',
+            'db_table': 'NrcParsedReport'
+        },
+
+        # -={ Table: NrcScrapedMaterials }=- #
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'SEQNOS',
+            'db_field': 'reportnum',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'CHRIS_CODE',
+            'db_field': 'chris_code',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'NAME_OF_MATERIAL',
+            'db_field': 'name',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'UPPER_BOUNDS',
+            'db_field': 'amount',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'UPPER_BOUNDS_UNITS',
+            'db_field': 'unit',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'IF_REACHED_WATER',
+            'db_field': 'reached_water',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'AMOUNT_IN_WATER',
+            'db_field': 'amt_in_water',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        },
+        {
+            'sheet': 'MATERIAL_INVOLVED_CR',
+            'field': 'UNIT_OF_MEASURE_IF_REACH_WATER',
+            'db_field': 'amt_in_water_unit',
+            'db_schema': 'public',
+            'db_table': 'NrcScrapedMaterials'
+        }
+    ]
+
+
+    #/* ----------------------------------------------------------------------- */#
     #/*     Parse arguments
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
 
     i = 0
     arg_error = False
@@ -245,6 +469,41 @@ def main(args):
             elif arg in ('--license', '-usage'):
                 return print_license()
 
+            # Database connection
+            elif arg == '--db-connection-string':
+                i += 2
+                db_connection_string = args[i - 1]
+            elif arg == '-db-host':
+                i += 2
+                db_host = args[i - 1]
+            elif arg == '-db-user':
+                i += 2
+                db_user = args[i - 1]
+            elif arg == '-db-name':
+                i += 2
+                db_name = args[i - 1]
+            elif arg == '-db-pass':
+                i += 2
+                db_pass = args[i - 1]
+
+            # User defined field map: NrcParsedReport
+            elif '-field-map-NrcParsedReport' in arg:
+                i += 2
+                try:
+                    field_map_NrcParsedReport = json.loads(args[i - 1])
+                except ValueError:
+                    arg_error = True
+                    print("ERROR: Field map for NrcParsedReport Report is not JSON compliant")
+
+            # User defined field map: NrcScrapedMaterials
+            elif '-field-map-NrcScrapedMaterials' in arg:
+                i += 2
+                try:
+                    field_map_NrcScrapedMaterials = json.loads(args[i - 1])
+                except ValueError:
+                    arg_error = True
+                    print("ERROR: Field map for NrcScrapedMaterials is not JSON compliant")
+
             # Positional arguments and errors
             else:
 
@@ -253,18 +512,45 @@ def main(args):
                 arg_error = True
                 print("ERROR: Invalid argument: %s" % arg)
 
+        # This catches three conditions:
+        #   1. The last argument is a flag that requires parameters but the user did not supply the parameter
+        #   2. The arg parser did not properly consume all parameters for an argument
+        #   3. The arg parser did not properly iterate the 'i' variable
         except IndexError:
             i += 1
             arg_error = True
             print("ERROR: An argument has invalid parameters")
 
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
     #/*     Validate parameters
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
 
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
+    #/*     Prep DB connection and XLRD workbook for processing
+    #/* ----------------------------------------------------------------------- */#
+
+    # Allow user to provide a complete DB connection string
+    if db_connection_string is None:
+        db_connection_string = "host='%s' dbname='%s' user='%s' password='%s'" % (db_host, db_name, db_user, db_pass)
+
+    # Connect to DB
+    with psycopg2.connect(db_connection_string) as db_conn:
+
+        # Establish DB cursor using the DictCursor so that queries are received as a dict
+        with db_conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as db_cursor:
+
+            # Prep workbook
+            with xlrd.open_workbook(file_to_process) as workbook:
+
+                #/* ----------------------------------------------------------------------- */#
+                #/*     Prep workbook for processing
+                #/* ----------------------------------------------------------------------- */#
+
+                for sheet_name in ()
+
+    #/* ----------------------------------------------------------------------- */#
     #/*     Cleanup and final return
-    #/* ======================================================================= */#
+    #/* ----------------------------------------------------------------------- */#
 
     # Success
     return 0
