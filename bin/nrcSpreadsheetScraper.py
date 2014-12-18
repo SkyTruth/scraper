e1d07b5616ce3bb47da057279664eb95932d3d61
Paul Woods <pwoods25443@gmail.com>
2014-12-18 12:14:37 -0500

    bugfix in string literal

--- bin/nrcSpreadsheetScraper.py
+++ bin/nrcSpreadsheetScraper.py
@@ -379,7 +379,7 @@ def report_exists(**kwargs):
     # Perhpas have a report_exists method on each of the field map classes so we don't have to
     # have the same existance test for all tables
 
-    if table=='BotTaskStatus':
+    if table=='"BotTaskStatus"':
         cursor.execute("""SELECT * FROM %s.%s WHERE bot='NrcExtractor' AND task_id = %s""" % (schema, table, reportnum))
     else:
         cursor.execute("""SELECT * FROM %s.%s WHERE %s = %s""" % (schema, table, field, reportnum))
